8f221c4856e6143909881c733da8907b1badaaf0
Leandro Dos Santos Afonso <leandro.afonso@hotmail.com>
2022-06-12 18:38:21 -0400

    Completed Assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {qList, setQList} from "./QuestionList";
 
 function QuestionForm(props) {
   const [formData, setFormData] = useState({
@@ -20,6 +21,27 @@ function QuestionForm(props) {
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
+
+    const postData = {
+      method: 'POST',
+      headers: { 
+        "Content-Type": "application/json",
+       },
+      body: JSON.stringify({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4 
+        ],
+        correctIndex: parseInt(formData.correctIndex),
+      }),
+    }
+
+    fetch("http://localhost:4000/questions", postData)
+    .then((response) => response.json())
+    .then((item) => console.log(item))
   }
 
   return (
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,58 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
+
 
 function QuestionList() {
+  const [qList, setQList] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((response) => response.json())
+    .then((items) => setQList(items))
+  }, [])
+
+  function handleDeleteUpdate(id){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE", 
+    })
+    .then((response) => response.json())
+    .then(() => {
+      const udpatedList = qList.filter((q) => q.id !== id)
+      setQList(udpatedList)
+    })
+  }
+
+  function handleAnswerChange(id, correctIndex){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({correctIndex})
+
+    })
+    .then((response) => response.json())
+    .then((answer) => {
+      const updatedAnswers = qList.map((q) => {
+        if (q.id === answer.id) return answer;
+        return q;
+      });
+      setQList(updatedAnswers);
+    });
+
+  }
+
+
+  const displayQuestions = qList.map((q) => {
+    return <QuestionItem key={q.id} question={q} onDelete={handleDeleteUpdate} onChangeAnswer={handleAnswerChange}/>
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {displayQuestions}
+        </ul>
     </section>
   );
 }
